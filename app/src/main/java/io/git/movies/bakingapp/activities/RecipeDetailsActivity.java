b1e7492fd4e52f23f41f11fd4bec51410bc731a5
Gergely Agnecz <agnegerg@gmail.com>
2018-05-31 00:42:57 +0200

    Corrected the POJOs, added CardView based ingredient view

--- app/src/main/java/io/git/movies/bakingapp/activities/RecipeDetailsActivity.java
+++ app/src/main/java/io/git/movies/bakingapp/activities/RecipeDetailsActivity.java
@@ -9,19 +9,29 @@ import io.git.movies.bakingapp.R;
 import io.git.movies.bakingapp.pojos.Recipe;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
-    private TextView recipeNameTv;
+    private TextView recipeIngredientsTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Recipe recipe = getIntent().getExtras().getParcelable("Recipe");
         setContentView(R.layout.activity_recipe_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(recipe.getName());
         setSupportActionBar(toolbar);
 
-        Recipe recipe = getIntent().getExtras().getParcelable("Recipe");
+        recipeIngredientsTv = findViewById(R.id.recipeIngredientsTextView);
+        recipeIngredientsTv.setText(getIngredients(recipe));
+    }
 
-        recipeNameTv = findViewById(R.id.recipeNameTv);
-        recipeNameTv.setText(recipe.getName());
-
+    private String getIngredients(Recipe recipe) {
+        String retVal = "";
+        for (int i = 0; i < recipe.getListOfIngredients().size(); i++) {
+            String ingredientDetails = recipe.getListOfIngredients().get(i).getQuantity() + " " +
+                                       recipe.getListOfIngredients().get(i).getMeasure() + " of " +
+                                       recipe.getListOfIngredients().get(i).getIngredient() + ",\n";
+            retVal = retVal + ingredientDetails;
+        }
+        return retVal.substring(0, retVal.length() - 2);
     }
 }
